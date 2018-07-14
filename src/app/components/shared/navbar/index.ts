74ab3feb4d76137403beb81d42710a6027258b2c
Huiren Woo <giantcrabby@gmail.com>
2018-07-14 23:12:51 +0800

    Add nav bar module

---
+++ src/app/components/shared/navbar/index.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './index.html',
+  styleUrls: ['./index.scss']
+})
+export class NavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
