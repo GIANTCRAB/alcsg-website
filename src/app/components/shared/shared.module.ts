1d957b40d32e1d051addb711f222d928dfb2fe43
Huiren Woo <giantcrabby@gmail.com>
2018-07-15 00:28:21 +0800

    Change appearance of navbar

--- src/app/components/shared/shared.module.ts
+++ src/app/components/shared/shared.module.ts
@@ -1,12 +1,17 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
+import {RouterModule} from '@angular/router';
+import {routes} from './routes';
 import {NavbarComponent} from './navbar';
 
 @NgModule({
   imports: [
     CommonModule,
-    FontAwesomeModule
+    NgbModule,
+    FontAwesomeModule,
+    RouterModule.forChild(routes)
   ],
   exports: [
     FontAwesomeModule,
