09b597f6a5ed0ec2dacf84b2fc3affa0d8aad365
Huiren Woo <giantcrabby@gmail.com>
2018-07-20 21:56:34 +0800

    Add use of loading spinner

--- src/app/components/events/events.module.ts
+++ src/app/components/events/events.module.ts
@@ -3,10 +3,12 @@ import {CommonModule} from '@angular/common';
 import {NavigatorComponent} from './navigator';
 import {RouterModule} from '@angular/router';
 import {routes} from './routes';
+import {SharedModule} from '../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     RouterModule.forChild(routes)
   ],
   declarations: [NavigatorComponent]
