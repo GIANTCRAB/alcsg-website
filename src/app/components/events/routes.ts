71ab407c7a22a5043adc21ca4e27e06778b9331f
Huiren Woo <giantcrabby@gmail.com>
2018-11-10 13:42:48 +0800

    Add event listing route

--- src/app/components/events/routes.ts
+++ src/app/components/events/routes.ts
@@ -1,7 +1,12 @@
 import {Routes} from '@angular/router';
 import {NavigatorComponent} from './navigator';
+import {EventsListComponent} from './list';
 
 export const routes: Routes = [
+  {
+    path: '',
+    component: EventsListComponent
+  },
   {
     path: ':id',
     component: NavigatorComponent
